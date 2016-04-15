ef29c3e7c5d1a08fa0bc68e448b7affd6dc4d5b6
Chih-Kuang, Wu <shangoyanyi@gmail.com>
2016-04-15 07:03:44 +0000

    modify main.js to deploy on heroku

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-// var FollowOnSpotify = require("./components/FollowOnSpotify");
+var wurl = require('wurl');
 var SpotifyBox = require("./components/SpotifyBox");
 
-
-// ReactDOM.render(
-//   <FollowOnSpotify />, 
-//   document.getElementById("content")
-// );
+// 取得protocal跟hostname
+var protocol = wurl('protocol');
+var hostname = wurl('hostname');
 
 
+// 設定SpotifyBox需要的參數
 var clientId    = "349685b76abc4bb6a32ed4806fd39382";
-var redirectUri = "https://follow-xxx-on-spotify-shangoyanyi.c9users.io/spotify-oauth-proxy.html";
+// var redirectUri = "https://follow-xxx-on-spotify-shangoyanyi.c9users.io/spotify-oauth-proxy.html";
+var redirectUri = protocol + "://" + hostname + "/spotify-oauth-proxy.html"; // heroku跟cloud9的domain不同
 var scope = [
   'user-read-email',
   'user-read-private',
   'user-follow-read',
   'user-follow-modify'
-].join(' ');
+].join(' '); // 需要使用者授予的權限
 
 
 
